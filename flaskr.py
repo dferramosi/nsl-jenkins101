96b704765f32415fd5fbc64dc8e157c290d2ccca
Dominic Ferrantelli <dominic@ferrantel.li>
2015-09-26 16:36:11 -0700

    removing exception

--- flaskr.py
+++ flaskr.py
@@ -34,7 +34,6 @@ app.config.from_envvar('FLASKR_SETTINGS', silent=True)
 def connect_db():
     """Connects to the specific database."""
     rv = sqlite3.connect(app.config['DATABASE'])
-    raise Exception("crap")
     rv.row_factory = sqlite3.Row
     return rv
 
